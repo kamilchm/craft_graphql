28dbcdfeba5e3188d8fd0adc97962394914c19b2
Kamil Chmielewski <kamil.chm@gmail.com>
2018-02-08 08:54:14 +0100

    chapter 3 wip

---
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,144 @@
+#---
+# Excerpted from "Craft GraphQL APIs in Elixir with Absinthe",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/wwgraphql for more book information.
+#---
+defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase, async: true
+  import AssertValue
+
+  setup do
+    Code.load_file("priv/repo/seeds.exs")
+  end
+
+  @query """
+  {
+    menuItems {
+      name
+    }
+  }
+  """
+  test "menuItems field returns menu items" do
+    conn = build_conn()
+    conn = get conn, "/api", query: @query
+    assert json_response(conn, 200) == %{
+      "data" => %{
+        "menuItems" => [
+          %{"name" => "Bánh mì"},
+          %{"name" => "Chocolate Milkshake"},
+          %{"name" => "Croque Monsieur"},
+          %{"name" => "French Fries"},
+          %{"name" => "Lemonade"},
+          %{"name" => "Masala Chai"},
+          %{"name" => "Muffuletta"},
+          %{"name" => "Papadum"},
+          %{"name" => "Pasta Salad"},
+          %{"name" => "Rueben"},
+          %{"name" => "Soft Drink"},
+          %{"name" => "Vada Pav"},
+          %{"name" => "Vanilla Milkshake"},
+          %{"name" => "Water"},
+        ]
+      }
+    }
+  end
+
+  @query """
+  {
+    menuItems(filter: {name: "rue"}) {
+      name
+    }
+  }
+  """
+  test "menuItems field returns menu items filtered by name" do
+    response = get(build_conn(), "/api", query: @query)
+    assert json_response(response, 200) == %{
+      "data" => %{
+        "menuItems" => [
+          %{"name" => "Rueben"},
+        ]
+      }
+    }
+  end
+
+  @query """
+  {
+    menuItems(filter: {name: 123}) {
+      name
+    }
+  }
+  """
+  test "menuItems field returns errors when using a bad value" do
+    response = get(build_conn(), "/api", query: @query)
+    assert %{"errors" => [
+      %{"message" => message}
+    ]} = json_response(response, 400)
+    assert_value message == """
+    Argument \"filter\" has invalid value {name: 123}.
+    In field \"name\": Expected type \"String\", found 123.<NOEOL>
+    """
+  end
+
+  @query """
+  query ($term: String) {
+    menuItems(filter: {name: $term}) {
+      name
+    }
+  }
+  """
+  @variables %{"term" => "rue"}
+  test "menuItems field filters by name when using a variable" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+    assert json_response(response, 200) == %{
+      "data" => %{
+        "menuItems" => [
+          %{"name" => "Rueben"},
+        ]
+      }
+    }
+  end
+
+  @query """
+  query ($order: SortOrder!) {
+    menuItems(order: $order) {
+      name
+    }
+  }
+  """
+  @variables %{"order" => "DESC"}
+  test "menuItems failed returns menuItems descending when asked using literals" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+    assert %{
+      "data" => %{"menuItems" => [%{"name" => "Water"} | _]}
+    } = json_response(response, 200)
+  end
+
+  @query """
+  {
+    menuItems(filter: {category: "Sandwiches", tag: "Vegetarian"}) {
+      name
+    }
+  }
+  """
+  test "menuItems field returns menuItems, filtering with literal" do
+    response = get(build_conn(), "/api", query: @query)
+    assert_value json_response(response, 200) == %{"data" => %{"menuItems" => [%{"name" => "Vada Pav"}]}}
+  end
+
+  @query """
+  query ($filter: MenuItemFilter!) {
+    menuItems(filter: $filter) {
+      name
+    }
+  }
+  """
+  @variables %{filter: %{"tag" => "Vegetarian", "category" => "Sandwiches"}}
+  test "menuItems field returns menuItems, filtering with variable" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+    assert_value json_response(response, 200) == %{"data" => %{"menuItems" => [%{"name" => "Vada Pav"}]}}
+  end
+
+end
